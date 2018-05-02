b74cfab6c38f0c7e75f502763d6347dee08e303c
Marcos Zarraga <a01271642@itesm.mx>
2018-05-01 21:20:52 -0500

    Vista servicios 100% funcional

--- app/components/agregar-servicio.js
+++ app/components/agregar-servicio.js
@@ -1,11 +1,35 @@
 import Ember from 'ember';
+import { inject as service } from "@ember/service";
+import {computed} from '@ember/object';
+import {isBlank} from '@ember/utils';
 
 export default Ember.Component.extend({
+	session: service(),
+  firebase: service('firebaseApp'),
+
+	newServicio: computed('store', function(){
+		return this.get('store').createRecord('service')
+	}),
+
 	actions : {
 		addServicio(servicio){
+			servicio.set('unidadHab', this.get('currentUnit'));
 			servicio.save().then(()=>{
-				this.sendAction('addServicio')
+				this.get('currentUnit.servicios').then((serviceList)=>{
+					serviceList.pushObject(servicio)
+					serviceList.save().then(()=>{
+						this.get('currentUnit').then((currentUnit)=>{
+							currentUnit.save().then(()=>{
+								this.sendAction('addServicio')
+							})
+						})
+					})
+				})
 			})
+		},
+
+		cancel(){
+			this.sendAction('addServicio')
 		}
 	}
 });
--- app/controllers/servicios.js
+++ app/controllers/servicios.js
@@ -3,23 +3,42 @@ import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default Ember.Controller.extend({
-	store: service(),
+	currentUser: service(),
+  store: service(),
 
 	addNewServicio: computed('newServicio', function(){
 		return this.get('newServicio')
 	}),
 
-	newServicio: computed('store', function(){
-		return this.store.createRecord('service')
-	}),
-
-	allServicios: computed('store', function(){
-		return this.store.findAll('service')
+	currentUnit: computed('store', 'currentUser', function(){
+		return DS.PromiseObject.create({
+			promise: this.get('currentUser.account').then((account)=>{
+				return this.get('store').findRecord('unit', account.get('currentUnit')).then(function(theUnit){
+					// console.log(theUnit)
+					return theUnit;
+				})
+			})
+		});
 	}),
 
 	actions: {
 		addServicio(){
-			this.set('addNewServicio', false)
+			if(this.get('addNewServicio')) {
+				this.set('addNewServicio', false)
+			} else {
+				this.set('addNewServicio', true)
+			}
+		},
+
+		signOut(){
+			this.get('currentUser.account').then((account)=>{
+				account.set('currentUnit', null)
+				account.save().then(()=> {
+					this.get('session').close();
+					this.transitionToRoute('login');
+						 console.log('Sesion cerrada');
+				})
+			})
 		}
 	}
 });
