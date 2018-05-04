17cc12190c457d6add87393e3a9b27a5ac2b256c
Marcos Zarraga <a01271642@itesm.mx>
2018-05-04 04:40:39 -0500

    Alertas, solicitudes y encuestas

--- app/controllers/solicitudes.js
+++ app/controllers/solicitudes.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
+import {computed} from '@ember/object';
+import {inject as service} from '@ember/service';
+import {isBlank} from '@ember/utils';
 
 export default Ember.Controller.extend({
+  currentUser: service(),
+  store: service(),
+
+  currentUnit: computed('store', 'currentUser', function(){
+    return DS.PromiseObject.create({
+      promise: this.get('currentUser.account').then((account)=>{
+        return this.get('store').findRecord('unit', account.get('currentUnit')).then(function(theUnit){
+          // console.log(theUnit)
+          return theUnit;
+        })
+      })
+    });
+  }),
+  
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
+import {inject as service} from "@ember/service";
+import { isBlank } from '@ember/utils';
 
 export default Ember.Route.extend({
+      session: service(),
+      currentUser: service(),
+
+      beforeModel(){
+        this.get('currentUser.account').then((account)=>{
+          // console.log(account)
+          // console.log(isBlank(account))
+          // debugger
+          if(isBlank(account)){
+            if(this.get('session.isAuthenticated')){
+                this.get('session').close();
+            }
+            this.transitionTo('login')
+          } else {
+            this.transitionTo('inicio')
+          }
+        })
+      }
 });
