7fe2e210e280d692a25e0d744bd5b64b55bf8faa
Marcos Zarraga <a01271642@itesm.mx>
2018-05-04 03:28:43 -0500

    Bootstrap

--- app/controllers/calendario.js
+++ app/controllers/calendario.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
+import {inject as service} from '@ember/service';
 
 export default Ember.Controller.extend({
+  currentUser: service(),
+
+  actions: {
+		signOut(){
+			this.get('currentUser.account').then((account)=>{
+				account.set('currentUnit', null)
+				account.save().then(()=> {
+					this.get('session').close();
+					this.transitionToRoute('login');
+						 console.log('Sesion cerrada');
+				})
+			})
+		}
+	}
 });
